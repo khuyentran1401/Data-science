106fc818e0f7f8ea99ca8d7986b15f70cee1695e
khuyentran1401 <khuyentran1476@gmail.com>
2021-01-20 07:23:58 -0600

    good functions example

--- python/good_functions/bad_code.py
+++ python/good_functions/bad_code.py
@@ -1,46 +1,56 @@
-import gdown
+import xml.etree.ElementTree as ET
 import zipfile
-
 from os import listdir
 from os.path import isfile, join
-import xml.etree.ElementTree as ET
+
+import gdown
+
 
 def main():
 
-    load_data(url='https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3',
-            output='Twitter.zip',
-            path_train='Data/train/en',
-            path_test='Data/test/en')
+    load_data(
+        url="https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3",
+        output="Twitter.zip",
+        path_train="Data/train/en",
+        path_test="Data/test/en",
+    )
 
 
 def load_data(url: str, output: str, path_train: str, path_test: str):
 
     # Download data from Google Drive
-    output = 'Twitter.zip'
-    gdown.download(url, output, quiet=False) 
+    output = "Twitter.zip"
+    gdown.download(url, output, quiet=False)
 
     # Unzip data
-    with zipfile.ZipFile(output, 'r') as zip_ref:
-        zip_ref.extractall('.')
+    with zipfile.ZipFile(output, "r") as zip_ref:
+        zip_ref.extractall(".")
 
     # Get train, test data files
-    tweets_train_files = [file for file in listdir(path_train) if isfile(join(path_train, file)) and file != "truth.txt"]
-    tweets_test_files = [file for file in listdir(path_test) if isfile(join(path_test, file)) and file != "truth.txt"]
+    tweets_train_files = [
+        file
+        for file in listdir(path_train)
+        if isfile(join(path_train, file)) and file != "truth.txt"
+    ]
+    tweets_test_files = [
+        file
+        for file in listdir(path_test)
+        if isfile(join(path_test, file)) and file != "truth.txt"
+    ]
 
     # Extract texts from each file
     t_train = []
     for file in tweets_train_files:
-        train_doc_1 =[r.text for r in ET.parse(join(path_train, file)).getroot()[0]]
-        t_train.append(' '.join(t for t in train_doc_1))
-    
-    
+        train_doc_1 = [r.text for r in ET.parse(join(path_train, file)).getroot()[0]]
+        t_train.append(" ".join(t for t in train_doc_1))
+
     t_test = []
     for file in tweets_test_files:
-        test_doc_1 =[r.text for r in ET.parse(join(path_test, file)).getroot()[0]]
-        t_test.append(' '.join(t for t in test_doc_1))
-    
+        test_doc_1 = [r.text for r in ET.parse(join(path_test, file)).getroot()[0]]
+        t_test.append(" ".join(t for t in test_doc_1))
 
     return t_train, t_test
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
--- python/good_functions/good_code.py
+++ python/good_functions/good_code.py
@@ -1,26 +1,27 @@
-import gdown
+import xml.etree.ElementTree as ET
 import zipfile
-
 from os import listdir
 from os.path import isfile, join
-import xml.etree.ElementTree as ET
+from typing import List, Tuple
 
-from typing import Tuple, List
+import gdown
+
 
 def main():
 
-    url = 'https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3'
-    output_path = 'Twitter.zip'
-    path_train = 'Data/train/en'
-    path_test = 'Data/test/en'
+    url = "https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3"
+    output_path = "Twitter.zip"
+    path_train = "Data/train/en"
+    path_test = "Data/test/en"
 
     data_getter = DataGetter(url, output_path, path_train, path_test)
 
     tweet_train, tweet_test = data_getter.get_train_test_docs()
 
+
 class DataGetter:
     def __init__(self, url: str, output_path: str, path_train: str, path_test: str):
-        self.url = url 
+        self.url = url
         self.output_path = output_path
         self.path_train = path_train
         self.path_test = path_test
@@ -28,31 +29,40 @@ class DataGetter:
         self.unzip_data()
 
     def download_zip_data_from_google_drive(self):
-        
-        gdown.download(self.url, self.output_path, quiet=False) 
+
+        gdown.download(self.url, self.output_path, quiet=False)
 
     def unzip_data(self):
 
-        with zipfile.ZipFile(self.output_path, 'r') as zip_ref:
-            zip_ref.extractall('.')
+        with zipfile.ZipFile(self.output_path, "r") as zip_ref:
+            zip_ref.extractall(".")
 
     def get_train_test_docs(self) -> Tuple[list, list]:
 
         tweets_train_files = self.get_files(self.path_train)
         tweets_test_files = self.get_files(self.path_test)
 
-        t_train = self.extract_texts_from_multiple_files(self.path_train, tweets_train_files)
-        t_test  = self.extract_texts_from_multiple_files(self.path_test, tweets_test_files)
+        t_train = self.extract_texts_from_multiple_files(
+            self.path_train, tweets_train_files
+        )
+        t_test = self.extract_texts_from_multiple_files(
+            self.path_test, tweets_test_files
+        )
         return t_train, t_test
-
 
     @staticmethod
     def get_files(path: str) -> List[str]:
 
-        return [file for file in listdir(path) if isfile(join(path, file)) and file != "truth.txt"]
+        return [
+            file
+            for file in listdir(path)
+            if isfile(join(path, file)) and file != "truth.txt"
+        ]
 
     @classmethod
-    def extract_texts_from_multiple_files(cls, path_to_file: str, files: list) -> List[str]:
+    def extract_texts_from_multiple_files(
+        cls, path_to_file: str, files: list
+    ) -> List[str]:
 
         all_docs = []
         for file in files:
@@ -63,11 +73,14 @@ class DataGetter:
 
     @staticmethod
     def extract_texts_from_each_file(path_to_file: str, file_name: list) -> str:
-        
-        list_of_text_in_one_file =[r.text for r in ET.parse(join(path_to_file, file_name)).getroot()[0]]
-        text_in_one_file_as_string = ' '.join(t for t in list_of_text_in_one_file)
-        
+
+        list_of_text_in_one_file = [
+            r.text for r in ET.parse(join(path_to_file, file_name)).getroot()[0]
+        ]
+        text_in_one_file_as_string = " ".join(t for t in list_of_text_in_one_file)
+
         return text_in_one_file_as_string
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()

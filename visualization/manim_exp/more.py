98d63582b87a178d756405fe5da963d447747c1b
khuyentran1401 <khuyentran1476@gmail.com>
2021-04-05 15:18:51 -0500

    add manim

--- visualization/manim_exp/more.py
+++ visualization/manim_exp/more.py
@@ -121,3 +121,20 @@ class GroupCircles(Scene):
         self.play(circle_red.animate.shift(RIGHT))
         self.wait()
 
+class TracedPathExample(Scene):
+    def construct(self):
+        # Create circle and dot
+        circ = Circle(color=BLUE).shift(4*LEFT)
+        dot = Dot(color=BLUE).move_to(circ.get_start())
+
+        # Group dot and circle
+        rolling_circle = VGroup(circ, dot)
+        trace = TracedPath(circ.get_start)
+
+        rolling_circle.add_updater(lambda m: m.rotate(-0.3))  # Rotate the circle
+
+        self.add(trace, rolling_circle) # add trace and rolling circle to the scene
+
+        # Shift the circle to 8*RIGHT
+        self.play(rolling_circle.animate.shift(8*RIGHT), run_time=4, rate_func=linear)
+
--- visualization/manim_exp/start.py
+++ visualization/manim_exp/start.py
@@ -2,6 +2,8 @@ from manim import *
 
 config.background_color = DARK_GRAY
 
+from manim import * 
+
 class PointMovingOnShapes(Scene):
     def construct(self):
         
@@ -14,28 +16,10 @@ class PointMovingOnShapes(Scene):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
         
-        # Create a dot
-        dot = Dot()
-        dot2 = dot.copy().shift(RIGHT) # create a copy of the dot and shift
-                                       # it to the right
-        self.add(dot) # Add the dot to the scene
-        
-         # Create a line next to the dot
-        line = Line([3, 0, 0], [5, 0, 0])
-        self.add(line)
-
-        
         # Create animations
         self.play(GrowFromCenter(square))
         self.play(Transform(square, circle)) 
-        
-        # shift the dot to the right
-        self.play(Transform(dot, dot2))
-        
-         # rotates the dot abount point (2,0,0) for 1.5 s
-        self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
        
         self.wait()
-
 
 

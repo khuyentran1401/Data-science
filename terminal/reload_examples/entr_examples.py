5ec12a06f30ae8ab2ef65a965beab361708d0920
khuyentran1401 <khuyentran1476@gmail.com>
2021-05-08 08:00:20 -0500

    add reloading

--- terminal/reload_examples/entr_examples.py
+++ terminal/reload_examples/entr_examples.py
@@ -3,3 +3,7 @@ def get_evens(nums: list):
 
 def test_get_evens():
     assert get_evens([1, 3, 4, 6, 8, 9]) == [4, 6, 8]
+
+"""On your terminal
+ls entr_examples.py | entr python entr_examples.py 
+"""

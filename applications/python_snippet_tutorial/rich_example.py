52965759c6bb48e65ab85964b8fde1ae13d8655d
Khuyen <khuyentran1476@gmail.com>
2021-06-29 20:19:40 -0500

    add rich example

--- applications/python_snippet_tutorial/rich_example.py
+++ applications/python_snippet_tutorial/rich_example.py
@@ -4,11 +4,13 @@ from rich.syntax import Syntax
 console = Console()
 
 code = """
-import numpy as np
-import pandas as pd
-
-a = np.array([1, 2, 3, 4, 5])
-
-df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+import pandas as pd                                                              
+                                                                                 
+df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})                              
+                                                                                 
+df = (df.assign(col3=lambda x: x.col1 * 100 + x.col2)                            
+    .assign(col4=lambda x: x.col2 * x.col3)                                      
+    )                                                                            
+print(df)                                                                        
 """
 console.print(Syntax(code, 'python'))

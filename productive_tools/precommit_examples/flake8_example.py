d34f2271bbff07fd28ee9446b2ccf709863e853d
Khuyen <khuyentran1476@gmail.com>
2021-07-24 09:17:58 -0500

    add precommit examples

---
+++ productive_tools/precommit_examples/flake8_example.py
@@ -0,0 +1,6 @@
+def very_long_function_name(var1, var2, var3, var4, var5):
+    """A very long function"""
+    print(var1, var2, var3, var4, var5)
+
+
+very_long_function_name(1, 2, 3, 4, 5)
---
+++ productive_tools/precommit_examples/isort_example.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from flake8_example import very_long_function_name
+from sklearn.linear_model import (
+    LinearRegression,
+    LinearRegressionCV,
+    LogisticRegression,
+    LogisticRegressionCV,
+    OrderedLogisticRegression,
+)
+from sklearn.model_selection import train_test_split

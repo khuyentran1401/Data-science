e1ae9d95756fced7bba026959d1119add2290f2b
khuyentran1401 <khuyentran1476@gmail.com>
2022-02-09 10:44:06 -0600

    revert the changes of prefect

--- data_science_tools/prefect_example/data_engineering.py
+++ data_science_tools/prefect_example/data_engineering.py
@@ -4,7 +4,6 @@ import pandas as pd
 from prefect import Flow, Parameter, task
 from prefect.engine.results import LocalResult
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder
 
 
 # ---------------------------------------------------------------------------- #
@@ -15,13 +14,20 @@ def load_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
+@task(
+    target="{date:%a_%b_%d_%Y_%H-%M-%S}/{task_name}_output",
+    result=LocalResult(dir="data/processed"),
+)
+def get_classes(data: pd.DataFrame, target_col: str) -> List[str]:
+    """Task for getting the classes from the Iris data set."""
+    return sorted(data[target_col].unique())
+
+
 @task
 def encode_categorical_columns(data: pd.DataFrame, target_col: str) -> pd.DataFrame:
     """Task for encoding the categorical columns in the Iris data set."""
 
-    enc = OrdinalEncoder()
-    data[target_col] = enc.fit_transform(data[[target_col]])
-    return data
+    return pd.get_dummies(data, columns=[target_col], prefix="", prefix_sep="")
 
 
 @task(
@@ -29,14 +35,16 @@ def encode_categorical_columns(data: pd.DataFrame, target_col: str) -> pd.DataFr
     target="{date:%a_%b_%d_%Y_%H-%M-%S}/{task_name}_output",
     result=LocalResult(dir="data/processed"),
 )
-def split_data(data: pd.DataFrame, test_data_ratio: float) -> Dict[str, Any]:
+def split_data(
+    data: pd.DataFrame, test_data_ratio: float, classes: list
+) -> Dict[str, Any]:
     """Task for splitting the classical Iris data set into training and test
     sets, each split into features and labels.
     """
 
     print(f"Splitting data into training and test sets with ratio {test_data_ratio}")
 
-    X, y = data.drop(columns=target_col), data[target_col]
+    X, y = data.drop(columns=classes), data[classes]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_data_ratio)
 
     # When returning many variables, it is a good practice to give them names:
@@ -60,9 +68,10 @@ with Flow("data-engineer") as flow:
 
     # Define tasks
     data = load_data(path="data/raw/iris.csv")
+    classes = get_classes(data=data, target_col=target_col)
     categorical_columns = encode_categorical_columns(data=data, target_col=target_col)
     train_test_dict = split_data(
-        data=categorical_columns, test_data_ratio=test_data_ratio
+        data=categorical_columns, test_data_ratio=test_data_ratio, classes=classes
     )
 
 # flow.visualize()
--- data_science_tools/prefect_example/data_science.py
+++ data_science_tools/prefect_example/data_science.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 from prefect import Flow, Parameter, task
 from prefect.engine.results import LocalResult
-from sklearn.metrics import accuracy_score, f1_score
-from sklearn.neighbors import KNeighborsClassifier
 
 
 # ---------------------------------------------------------------------------- #
@@ -13,30 +11,59 @@ from sklearn.neighbors import KNeighborsClassifier
 def train_model(
     train_x: pd.DataFrame,
     train_y: pd.DataFrame,
-    n_neighbors: int,
+    num_train_iter: int,
+    learning_rate: float,
 ) -> np.ndarray:
 
+    num_iter = num_train_iter
+    lr = learning_rate
     X = train_x.to_numpy()
     Y = train_y.to_numpy()
 
-    # Create a new model instance
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    # Add bias to the features
+    bias = np.ones((X.shape[0], 1))
+    X = np.concatenate((bias, X), axis=1)
 
-    # Train the model
-    knn.fit(X, Y)
+    weights = []
+    # Train one model for each class in Y
+    for k in range(Y.shape[1]):
+        # Initialise weights
+        theta = np.zeros(X.shape[1])
+        y = Y[:, k]
+        for _ in range(num_iter):
+            z = np.dot(X, theta)
+            h = _sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            theta -= lr * gradient
+        # Save the weights for each model
+        weights.append(theta)
 
     # Print finishing training message
     print("Finish training the model.")
 
-    return knn
+    # Return a joint multi-class model with weights for all classes
+    return np.vstack(weights).transpose()
+
+
+def _sigmoid(z):
+    """A helper sigmoid function used by the training and the scoring tasks."""
+    return 1 / (1 + np.exp(-z))
 
 
 @task
-def predict(model: np.ndarray, X: pd.DataFrame) -> np.ndarray:
+def predict(model: np.ndarray, test_x: pd.DataFrame) -> np.ndarray:
     """Task for making predictions given a pre-trained model and a test set."""
-    X = X.to_numpy()
+    X = test_x.to_numpy()
 
-    return model.predict(X)
+    # Add bias to the features
+    bias = np.ones((X.shape[0], 1))
+    X = np.concatenate((bias, X), axis=1)
+
+    # Predict "probabilities" for each class
+    result = _sigmoid(np.dot(X, model))
+
+    # Return the index of the class with max probability for all samples
+    return np.argmax(result, axis=1)
 
 
 @task(log_stdout=True)
@@ -44,14 +71,12 @@ def report_accuracy(predictions: np.ndarray, test_y: pd.DataFrame) -> None:
     """Task for reporting the accuracy of the predictions performed by the
     previous task. Notice that this function has no outputs, except logging.
     """
-    target = test_y.to_numpy()
-
-    accuracy = accuracy_score(target, predictions)
-    f1 = f1_score(target, predictions, average="weighted")
-
-    # Log the metrics of the model
-    print(f"The model accuracy on test set: {round(accuracy * 100, 2)}")
-    print(f"The F1-score on test set: {round(f1 * 100, 2)}")
+    # Get true class index
+    target = np.argmax(test_y.to_numpy(), axis=1)
+    # Calculate accuracy of predictions
+    accuracy = np.sum(predictions == target) / target.shape[0]
+    # Log the accuracy of the model
+    print(f"Model accuracy on test set: {round(accuracy * 100, 2)}")
 
 
 # ---------------------------------------------------------------------------- #
@@ -61,7 +86,7 @@ def report_accuracy(predictions: np.ndarray, test_y: pd.DataFrame) -> None:
 with Flow("data-science") as flow:
 
     train_test_dict = (
-        LocalResult(dir="data/processed/Tue_Dec_21_2021_16-26-44/")
+        LocalResult(dir="data/processed/Mon_Dec_20_2021_20:55:20")
         .read(location="split_data_output")
         .value
     )
@@ -73,10 +98,11 @@ with Flow("data-science") as flow:
     test_y = train_test_dict["test_y"]
 
     # Define parameters
-    n_neighbors = Parameter("n_neighbors", default=12)
+    num_train_iter = Parameter("num_train_iter", default=10000)
+    learning_rate = Parameter("learning_rate", default=0.01)
 
     # Define tasks
-    model = train_model(train_x, train_y, n_neighbors=n_neighbors)
+    model = train_model(train_x, train_y, num_train_iter, learning_rate)
     predictions = predict(model, test_x)
     report_accuracy(predictions, test_y)
 

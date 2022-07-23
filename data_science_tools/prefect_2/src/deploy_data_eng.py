635ed57220abe52e94e2e87798471ca409196efc
Khuyen Tran <khuyentran1476@gmail.com>
2022-07-23 16:30:10 -0500

    fix textblob code

--- data_science_tools/prefect_2/src/deploy_data_eng.py
+++
@@ -1,9 +0,0 @@
-from prefect.deployments import DeploymentSpec
-from prefect.flow_runners import SubprocessFlowRunner
-
-DeploymentSpec(
-    name="data_engineer",
-    flow_location="./data_engineering.py",
-    parameters={"test_data_ratio": 0.2},
-    flow_runner=SubprocessFlowRunner(),
-)

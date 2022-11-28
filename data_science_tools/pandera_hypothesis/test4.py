1c0ee047267a128068472f18c12e5a7d78aacc33
Khuyen Tran <khuyentran1476@gmail.com>
2022-11-28 10:26:33 -0600

    edit functools example

--- data_science_tools/pandera_hypothesis/test4.py
+++ data_science_tools/pandera_hypothesis/test4.py
@@ -1,26 +1,29 @@
-import hypothesis
+import pandas as pd
+import pytest
 import pandera as pa
+import hypothesis
 
 schema = pa.DataFrameSchema(
     {
         "val1": pa.Column(int, pa.Check.in_range(-2, 3)),
         "val2": pa.Column(int, pa.Check.in_range(-2, 3)),
     }
-)
+) 
 
-out_schema = schema.add_columns(
+expected = pa.DataFrameSchema(
     {
+        "val1": pa.Column(int, pa.Check.in_range(-2, 3)),
+        "val2": pa.Column(int, pa.Check.in_range(-2, 3)),
         "val3": pa.Column(float, pa.Check.in_range(-2, 3)),
-    },
+    }
 )
 
-
-@pa.check_output(out_schema)
+@pa.check_output(expected)
 def processing_fn(df):
     processed = df.assign(val3=df.val1 / df.val2)
     return processed
 
-
 @hypothesis.given(schema.strategy(size=5))
-def test_processing_fn(dataframe):
-    processing_fn(dataframe)
+def test_processing_fn(df):
+    # Get result
+    result = processing_fn(df)

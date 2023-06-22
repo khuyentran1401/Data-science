9e1e7acec3cd94cbcceb03d4206e968e79d53909
khuyentran1401 <khuyentran1476@gmail.com>
2023-06-22 16:52:06 -0500

    edit best practices for python functions

--- python/good_functions/good_code.py
+++ python/good_functions/good_code.py
@@ -1,77 +1,77 @@
 import xml.etree.ElementTree as ET
 import zipfile
-from os import listdir
-from os.path import isfile, join
-from typing import List, Tuple
+from pathlib import Path
+from typing import Tuple
 
 import gdown
+from pydantic import BaseModel
 
 
-def main():
-    url = "https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3"
-    output_path = "Twitter.zip"
-    path_train = "Data/train/en"
-    path_test = "Data/test/en"
-
-    data_getter = DataGetter(url, output_path, path_train, path_test)
-
-    tweet_train, tweet_test = data_getter.get_train_test_docs()
+class RawLocation(BaseModel):
+    url: str
+    zip_path: str
+    path_train: str
+    path_test: str
 
 
-class DataGetter:
-    def __init__(self, url: str, output_path: str, path_train: str, path_test: str):
-        self.url = url
-        self.output_path = output_path
-        self.path_train = path_train
-        self.path_test = path_test
-        self.download_zip_data_from_google_drive()
-        self.unzip_data()
+class ProcessedLocation(BaseModel):
+    path_train: str
+    path_test: str
 
-    def download_zip_data_from_google_drive(self):
-        gdown.download(self.url, self.output_path, quiet=False)
 
-    def unzip_data(self):
-        with zipfile.ZipFile(self.output_path, "r") as zip_ref:
-            zip_ref.extractall(".")
+def main(raw_location: RawLocation, processed_location: ProcessedLocation) -> None:
+    get_raw_data(raw_location)
+    t_train, t_test = get_train_test_docs(raw_location)
+    save_train_test_docs(processed_location, t_train, t_test)
 
-    def get_train_test_docs(self) -> Tuple[list, list]:
-        tweets_train_files = self.get_files(self.path_train)
-        tweets_test_files = self.get_files(self.path_test)
 
-        t_train = self.extract_texts_from_multiple_files(
-            self.path_train, tweets_train_files
-        )
-        t_test = self.extract_texts_from_multiple_files(
-            self.path_test, tweets_test_files
-        )
-        return t_train, t_test
+def get_raw_data(raw_location: RawLocation) -> None:
+    gdown.download(raw_location.url, raw_location.zip_path, quiet=False)
+    with zipfile.ZipFile(raw_location.zip_path, "r") as zip_ref:
+        zip_ref.extractall(".")
 
-    @staticmethod
-    def get_files(path: str) -> List[str]:
-        return [
-            file
-            for file in listdir(path)
-            if isfile(join(path, file)) and file != "truth.txt"
-        ]
 
-    @classmethod
-    def extract_texts_from_multiple_files(
-        cls, path_to_file: str, files: list
-    ) -> List[str]:
-        all_docs = []
-        for file in files:
-            text_in_one_file = cls.extract_texts_from_each_file(path_to_file, file)
-            all_docs.append(text_in_one_file)
+def get_train_test_docs(raw_location: RawLocation) -> Tuple[str, str]:
+    t_train = extract_texts_from_multiple_files(raw_location.path_train)
+    t_test = extract_texts_from_multiple_files(raw_location.path_test)
+    return t_train, t_test
 
-        return all_docs
 
-    @staticmethod
-    def extract_texts_from_each_file(path_to_file: str, file_name: list) -> str:
-        list_of_text_in_one_file = [
-            r.text for r in ET.parse(join(path_to_file, file_name)).getroot()[0]
-        ]
-        return " ".join(list_of_text_in_one_file)
+def extract_texts_from_multiple_files(folder_path: str) -> str:
+    all_docs = []
+    for file_path in Path(folder_path).glob("*.xml"):
+        text_in_one_file = extract_texts_from_each_file(file_path)
+        all_docs.append(text_in_one_file)
+
+    return " ".join(all_docs)
+
+
+def extract_texts_from_each_file(file_path: str) -> str:
+    list_of_text_in_one_file = [r.text for r in ET.parse(file_path).getroot()[0]]
+    return " ".join(list_of_text_in_one_file)
+
+
+def save_train_test_docs(
+    processed_location: ProcessedLocation, t_train: str, t_test: str
+) -> None:
+    save_data(processed_location.path_train, t_train)
+    save_data(processed_location.path_test, t_test)
+
+
+def save_data(processed_path: str, processed_data: str) -> None:
+    with open(processed_path, "w") as f:
+        f.write(processed_data)
 
 
 if __name__ == "__main__":
-    main()
+    raw_location = RawLocation(
+        url="https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3",
+        zip_path="Twitter.zip",
+        path_train="Data/train/en",
+        path_test="Data/test/en",
+    )
+    processed_location = ProcessedLocation(
+        path_train="Data/train/en.txt",
+        path_test="Data/test/en.txt",
+    )
+    main(raw_location, processed_location)

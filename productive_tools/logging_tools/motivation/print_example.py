7b42b7a0a65e75b47b11b7c193689919081f5860
khuyentran1401 <khuyentran1476@gmail.com>
2023-12-20 11:58:16 -0600

    add data science simplified banner

--- productive_tools/logging_tools/motivation/print_example.py
+++ productive_tools/logging_tools/motivation/print_example.py
@@ -1,21 +1,30 @@
 def encode_data(data: list):
-    print("Encode data")
     data_map = {"a": 1, "b": 2, "c": 3}
-    print(f"Data map: {data_map}")
     return [data_map[num] for num in data]
 
 
 def add_one(data: list):
-    print("Add one")
     return [num + 1 for num in data]
 
 
+def add_two(data: list):
+    return [num + 2 for num in data]
+
+
+def multiply_by_two(data: list):
+    return [num * 2 for num in data]
+
+
 def process_data(data: list):
-    print("Process data")
     data = encode_data(data)
     print(f"Encoded data: {data}")
     data = add_one(data)
     print(f"Added one: {data}")
+    data = add_two(data)
+    print(f"Added two: {data}")
+    data = multiply_by_two(data)
+    print(f"Multiplied by two: {data}")
+    return data
 
 
 process_data(["a", "a", "c"])

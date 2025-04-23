a8f9e2df080ac34488811e61770c6638130ec158
khuyentran1401 <khuyentran1476@gmail.com>
2025-04-23 10:19:21 -0500

    add marimo examples

---
+++ data_science_tools/marimo_examples/variable_redefinition.py
@@ -0,0 +1,32 @@
+import marimo
+
+__generated_with = "0.13.0"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    data = [1, 2, 3]
+    return (data,)
+
+
+@app.cell
+def _(data):
+    summary = sum(data)
+    print("Sum:", summary)
+    return
+
+
+@app.cell
+def _():
+    data_1 = [10, 20, 30]
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
